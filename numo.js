5ac4cb6966b1546243158444f2fc45c642b4baa6
Roman Borenko <roman.borenko@inclusive-it.com>
2025-07-30 20:30:08 +0300

    For Dariia #5

--- numo.js
+++ numo.js
@@ -3,6 +3,12 @@ function numo_messgr_fix() {
     for (let ies=0; ies<mess.length; ies++) {
         mess[ies].style.display = 'block';
     }
+    svgs = document.querySelectorAll('.header-messengers svg');
+    for (let ies=0; ies<svgs.length; ies++) {
+        svgs[ies].tabindex = '-1';
+        svgs[ies].role = 'none';
+        svgs[ies].ariaHidden = 'true';
+    }
     submess = document.querySelectorAll('.header-messengers > *');
     for (let ies=0; ies<submess.length; ies++) {
         submess[ies].style.display = 'inline';
